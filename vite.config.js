93504701476b3ad8aa07213525e243e5ce0326e2
lyn-kodehode <lyn-kodehode@gmail.com>
2025-09-18 14:03:39 +0200

    started gh-pages'
    git sta

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/lyn-michaels-vite-portfolio/", // Replace with your actual GitHub repo name
+  build: {
+    outDir: "dist",
+  },
+});
